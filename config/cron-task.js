fee03e895c7c5f057aa9f4d2840f9c33c475878f
karim-jaypay <karim@jaypayagency.com>
2021-12-09 10:07:35 +0200

    t

--- config/cron-task.js
+++ config/cron-task.js
@@ -1,15 +1,11 @@
 const axios = require('axios')
 
 module.exports = {
-    /**
-     * Simple example.
-     * Every monday at 1am.
-     */
+    
+
     // everyday at 12:00 am: 0 0 * * *   every 3 seconds: */3 * * * * *
-    //'*/3 * * * * *': ({ strapi }) => {
-      // Add your own logic here (e.g. send a queue of email, create a database backup, etc.).
-     // console.log('hi')
-    //},
+    
+    // update values of important quotes everyday at midnight
     '0 0 * * *': async ({ strapi }) => {
 
         // default quotes
@@ -51,13 +47,77 @@ module.exports = {
                     // update the existing row with new details
                     await strapi.entityService.update('api::quotes-default.quotes-default',result[0].id,{
                         data: {
-                        bid: data[title].bid,
-                        ask: data[title].ask,
+                        bid: (title === 'EURUSD' || title === 'GBPUSD') ? data[title].bid.toFixed(5) : data[title].bid,
+                        ask: (title === 'EURUSD' || title === 'GBPUSD') ? data[title].ask.toFixed(5) : data[title].ask,
                         }
                     }).catch((err) => console.log(err))
                 }
             }
         }
     },
-  };
+
+    // get trade cards live data
+    '*/3 * * * * *': async ({ strapi }) => {
+
+        // default quotes
+        const titles = ['EURUSD', 'USDJPY', 'GBPUSD', 'XAUUSD']
+    
+        // for every quote title run api
+        for( let i = 0; i < titles.length; i++) {
+
+            const title = titles[i]
+            // get quote of each title
+            const { data } = await axios.get(`http://summit-lb-tf-1076725243.eu-west-1.elb.amazonaws.com/quotes/${title}/`)
+            .catch((err) => console.log(err))
+           
+            if(data) {
+
+                // find and get old bid and old ask of any quote from quotes-defaults database
+                const old = await strapi.entityService.findMany('api::quotes-default.quotes-default',{
+                    filters: {
+                        title: title
+                    }
+                }).catch((err) => console.log(err))
+
+                const oldbid = old[0].bid
+                const oldask = old[0].ask
+        
+                // find if quote title exists on database
+                const result = await strapi.entityService.findMany('api::trade-card.trade-card',{
+                    filters: {
+                        title: title
+                    }
+                }).catch((err) => console.log(err))
+
+                // if it doesn't exist in database
+                if(result.length === 0 ) {
+
+                    // create a new row with quote details
+                    await strapi.entityService.create('api::trade-card.trade-card',{
+                        data: {
+                        title: title,
+                        bid: data[title].bid,
+                        ask: data[title].ask,
+                        oldbid: oldbid,
+                        oldask: oldask
+                        }
+                    }).catch((err) => console.log(err))
+
+                } else {
+
+                    // update the existing row with new details
+                    await strapi.entityService.update('api::trade-card.trade-card',result[0].id,{
+                        data: {
+                        bid: (title === 'EURUSD' || title === 'GBPUSD') ? data[title].bid.toFixed(5) : data[title].bid,
+                        ask: (title === 'EURUSD' || title === 'GBPUSD') ? data[title].ask.toFixed(5) : data[title].ask,
+                        oldbid: oldbid,
+                        oldask: oldask
+                        }
+                    }).catch((err) => console.log(err))
+                }
+            }
+        }
+    },
+
+};
    
