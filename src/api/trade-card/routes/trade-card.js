8b76ea406f90639544e461cf4fd1bd18d5c577d5
jaypay <karim@jaypayagency.com>
2021-12-09 01:44:17 -0600

    t

---
+++ src/api/trade-card/routes/trade-card.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * trade-card router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::trade-card.trade-card');
---
+++ src/api/trade-card/services/trade-card.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * trade-card service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::trade-card.trade-card');
