b31363fd30f764d5aad0476e0a283e879a87e03e
karim-jaypay <karim@jaypayagency.com>
2021-12-10 09:55:53 +0200

    t

--- config/cron-task.js
+++ config/cron-task.js
@@ -75,6 +75,9 @@ module.exports = {
     // get everestcm trade cards and forex, metal categories live data
     '*/3 * * * * *': async ({ strapi }) => {
 
+        // default trade cards quotes
+        const trade_titles = ['EURUSD', 'USDJPY', 'GBPUSD', 'XAUUSD']
+
         // default forex quotes
         const forex_titles = [
             'EURUSD', 
@@ -95,8 +98,18 @@ module.exports = {
             'XPTUSD',
             'XPDUSD'
         ]
-        // default trade cards quotes
-        const trade_titles = ['EURUSD', 'USDJPY', 'GBPUSD', 'XAUUSD']
+        
+        // default energies quotes
+        const energy_titles = [
+            'XBRUSD',
+            'XTIUSD'
+        ]
+
+        // default indices quotes
+        const indice_titles = [
+            'AUS200',
+            'UK100'
+        ]
 
         // for every trade card quote title run api
         for( let i = 0; i < trade_titles.length; i++) {
@@ -212,6 +225,8 @@ module.exports = {
                         oldask: oldask,
                         pips: (title === 'USDJPY') 
                         ? (ask - bid) * 100
+                        : (title === 'USDRUB')
+                        ? (ask - bid)
                         : (ask - bid) * 10000
                         }
                     }).catch((err) => console.log(err))
@@ -227,6 +242,8 @@ module.exports = {
                         oldask: oldask,
                         pips: (title === 'USDJPY') 
                         ? (ask - bid) * 100
+                        : (title === 'USDRUB')
+                        ? (ask - bid)
                         : (ask - bid) * 10000
                         }
                     }).catch((err) => console.log(err))
@@ -293,6 +310,128 @@ module.exports = {
                         pips: (title === 'XAGUSD') 
                         ? (ask - bid) * 100
                         : (ask - bid)
+                        }
+                    }).catch((err) => console.log(err))
+                }
+            }
+        }
+
+        // for every energies quote title run api
+        for( let i = 0; i < energy_titles.length; i++) {
+
+            const title = energy_titles[i]
+            // get quote of each title
+            const { data } = await axios.get(`http://summit-lb-tf-1076725243.eu-west-1.elb.amazonaws.com/quotes/${title}/`)
+            .catch((err) => console.log(err))
+           
+            if(data) {
+
+                const bid = data[title].bid
+                const ask = data[title].ask
+
+                // find and get old bid and old ask of any quote from quotes-defaults database
+                const old = await strapi.entityService.findMany('api::quotes-default.quotes-default',{
+                    filters: {
+                        title: title
+                    }
+                }).catch((err) => console.log(err))
+
+                const oldbid = old[0].bid
+                const oldask = old[0].ask
+        
+                // find if quote title exists on database
+                const result = await strapi.entityService.findMany('api::energy.energy',{
+                    filters: {
+                        title: title
+                    }
+                }).catch((err) => console.log(err))
+
+                // if it doesn't exist in database
+                if(result.length === 0 ) {
+
+                    // create a new row with quote details
+                    await strapi.entityService.create('api::energy.energy',{
+                        data: {
+                        title: title,
+                        bid: bid,
+                        ask: ask,
+                        oldbid: oldbid,
+                        oldask: oldask,
+                        pips: (ask - bid)
+                        }
+                    }).catch((err) => console.log(err))
+
+                } else {
+
+                    // update the existing row with new details
+                    await strapi.entityService.update('api::energy.energy',result[0].id,{
+                        data: {
+                        bid: bid,
+                        ask: ask,
+                        oldbid: oldbid,
+                        oldask: oldask,
+                        pips: (ask - bid)
+                        }
+                    }).catch((err) => console.log(err))
+                }
+            }
+        }
+
+        // for every forex quote title run api
+        for( let i = 0; i < indice_titles.length; i++) {
+
+            const title = indice_titles[i]
+            // get quote of each title
+            const { data } = await axios.get(`http://summit-lb-tf-1076725243.eu-west-1.elb.amazonaws.com/quotes/${title}/`)
+            .catch((err) => console.log(err))
+           
+            if(data) {
+
+                const bid = data[title].bid
+                const ask = data[title].ask
+
+                // find and get old bid and old ask of any quote from quotes-defaults database
+                const old = await strapi.entityService.findMany('api::quotes-default.quotes-default',{
+                    filters: {
+                        title: title
+                    }
+                }).catch((err) => console.log(err))
+
+                const oldbid = old[0].bid
+                const oldask = old[0].ask
+        
+                // find if quote title exists on database
+                const result = await strapi.entityService.findMany('api::indice.indice',{
+                    filters: {
+                        title: title
+                    }
+                }).catch((err) => console.log(err))
+
+                // if it doesn't exist in database
+                if(result.length === 0 ) {
+
+                    // create a new row with quote details
+                    await strapi.entityService.create('api::indice.indice',{
+                        data: {
+                        title: title,
+                        bid: bid,
+                        ask: ask,
+                        oldbid: oldbid,
+                        oldask: oldask,
+                        pips: (ask - bid)
+                        }
+                    }).catch((err) => console.log(err))
+
+                } else {
+
+                    // update the existing row with new details
+                    await strapi.entityService.update('api::indice.indice',result[0].id,{
+                        data: {
+                        bid: (title === 'EURUSD' || title === 'GBPUSD') ? bid.toFixed(5) : bid,
+                        ask: (title === 'EURUSD' || title === 'GBPUSD') ? ask.toFixed(5) : ask,
+                        oldbid: oldbid,
+                        oldask: oldask,
+                        pips: (ask - bid)
                         }
                     }).catch((err) => console.log(err))
                 }
