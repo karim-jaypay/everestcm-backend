3fdc98de9f789c8ca19de3eeeb83f537b677b9d8
karim-jaypay <karim@jaypayagency.com>
2021-12-13 10:20:35 +0200

    home

--- src/api/home/controllers/home.js
+++ src/api/home/controllers/home.js
@@ -6,4 +6,39 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::home.home');
+//module.exports = createCoreController('api::home.home');
+
+module.exports = createCoreController('api::home.home', ({ strapi }) => ({
+    async find(ctx) {
+        const populateList = [
+            'HomeSlider',
+            'HomeSlider.button',
+            'HomeSlider.image',
+            'HomeSlider.background',
+            'second',
+            'third',
+            'third_card',
+            'third_card.icon',
+            'fourth',
+            'fourth_image',
+            'fourth_card',
+            'fourth_card.icon',
+            'fifth',
+            'fifth_image',
+            'sixth',
+            'sixth_card',
+            'sixth_card.icon',
+            'seventh',
+            'background',
+            'seventh_image',
+            'pay_icon',
+            'pay_icon.icon'
+        ]
+        // Push any additional query params to the array
+        populateList.push(ctx.query.populate)
+        ctx.query.populate = populateList.join(',')
+
+        const content = await super.find(ctx)
+        return content
+    }
+}));
--- src/api/home/routes/home.js
+++ src/api/home/routes/home.js
@@ -6,4 +6,22 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::home.home');
+//module.exports = createCoreRouter('api::home.home');
+
+
+module.exports = createCoreRouter('api::home.home', {
+    prefix: '',
+    only: ['find'],
+    except: [],
+    config: {
+      find: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+      findOne: {},
+      create: {},
+      update: {},
+      delete: {},
+    },
+  });
