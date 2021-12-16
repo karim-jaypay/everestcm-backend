85899fef04b68e549747205e3ebafd9c58c8cd8e
karim-jaypay <karim@jaypayagency.com>
2021-12-16 14:26:20 +0200

    t

--- src/api/trade-card/controllers/trade-card.js
+++ src/api/trade-card/controllers/trade-card.js
@@ -6,4 +6,18 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::trade-card.trade-card');
+//module.exports = createCoreController('api::trade-card.trade-card');
+
+module.exports = createCoreController('api::trade-card.trade-card', ({ strapi }) => ({
+    async find(ctx) {
+        const populateList = [
+            'image'
+        ]
+        // Push any additional query params to the array
+        populateList.push(ctx.query.populate)
+        ctx.query.populate = populateList.join(',')
+
+        const content = await super.find(ctx)
+        return content
+    }
+}));
--- src/api/trade-card/routes/trade-card.js
+++ src/api/trade-card/routes/trade-card.js
@@ -6,4 +6,21 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::trade-card.trade-card');
+//module.exports = createCoreRouter('api::trade-card.trade-card');
+
+module.exports = createCoreRouter('api::trade-card.trade-card', {
+    prefix: '',
+    only: ['find'],
+    except: [],
+    config: {
+      find: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+      findOne: {},
+      create: {},
+      update: {},
+      delete: {},
+    },
+  });
